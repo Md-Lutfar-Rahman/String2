54e5f2f3b9371c93b8547914c8f13f580cc03654
Lutfar <lutfarrahmaninfo@gmail.com>
2023-02-04 10:10:12 +0600

    slice.js

---
+++ math.js
@@ -0,0 +1,12 @@
+const result = Math.pow(3,8);
+// console.log(result);
+
+const number1 = 25;
+const number2 = 45;
+const gap = Math.abs(number1-number2);
+if (gap<5){
+    console.log("Ok");
+}
+else{
+    console.log("False");
+}
---
+++ stringSearch.js
@@ -0,0 +1,8 @@
+    const myString = ("Hello This is Test Work, Done by Md.Lutfar Rahman");
+
+    console.log(myString.length);
+    const search = myString.includes("This");
+    console.log(search);
+
+    const stringSplit = myString.split(',');
+    console.log(stringSplit);
